05a0e5c7efc90ffbf97ee3a152808ad15dd15369
jordanmaharaj28 <jordanmaharaj10@gmail.com>
2026-04-17 12:32:19 -0400

    Checkpoint 1
    1st attempt at graph

--- B104_Final_Project.py
+++ B104_Final_Project.py
@@ -2,13 +2,38 @@
 # Course:     B104
 # Assignment: Final Project
 
+# import libraries 
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+# loads the YRBS data
 df = pd.read_csv('XXhq.txt', sep = '\t')
+ 
+print(df['q66'].head())
+print(df['q67'].head())
+print(df.columns)
 
-print(df.to_string())
+#convert q66 into numbers
+df["q66"] = pd.to_numeric(df["q66"], errors="coerce")
+#convert q67 into numbers
+df["q67"] = pd.to_numeric(df["q67"], errors="coerce")
 
-q33 = df['QN33']
+# correlation between question 66 and question 67
+df[["q66", "q67"]].corr()
+
+#countplot made
+sns.countplot(x="q66", hue="q67", data=df)
+
+# added a title and axis labels to better clarify it
+plt.title("Relationship Between Weight Perception (Q66) and Weight Goals (Q67)")
+plt.xlabel("Q66: Perception of Weight (1=Very Underweight... 5=Very Overweight)")
+plt.ylabel("Number of Students")
+# show the graph
+plt.show()
+
+
+
 
 
 
