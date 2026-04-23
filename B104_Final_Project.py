5844b7c1a531adc75f4dec9d7f0587e0fec72be0
jordanmaharaj28 <jordanmaharaj10@gmail.com>
2026-04-22 23:03:11 -0400

    GUI Attempt
    Added a simple gui that shows the graph

--- B104_Final_Project.py
+++ B104_Final_Project.py
@@ -6,6 +6,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import tkinter as tk
 
 # loads the YRBS data
 df = pd.read_csv('XXhq.txt', sep = '\t')
@@ -28,13 +29,51 @@ sns.countplot(x="q66",
               data=df, 
               palette=['green', 'skyblue', 'darkblue', 'orange']
               )
+# describes what each color in the small box means
+plt.legend(
+    title='Q67: Weight Goal', 
+    labels=['Lose Weight', 'Gain Weight', 'Stay The Same', 'Do Nothing']
+    )
 
 # added a title and axis labels to better clarify it
-plt.title("Relationship Between (Q66) and Weight Goals (Q67)")
+plt.title("Relationship Between Weight Perception (Q66) and Weight Goals (Q67)")
 plt.xlabel("Q66: Perception of Weight (1=Very Underweight to 5=Very Overweight)")
 plt.ylabel("Number of Students")
 # show the graph
 plt.show()
+
+# the function that runs when anyone presses the "Show Graph" button in the GUI
+def graph():
+    #countplot made with bars and customized colors
+    sns.countplot(x="q66", 
+                  hue="q67", 
+                  data=df, 
+                  palette=['green', 'skyblue', 'darkblue', 'orange']
+                  )
+    # describes what each color in the small box means
+    plt.legend(
+        title='Q67: Weight Goal', 
+        labels=['Lose Weight', 'Gain Weight', 'Stay The Same', 'Do Nothing']
+        )
+
+    # added a title and axis labels to better clarify it
+    plt.title("Relationship Between Weight Perception (Q66) and Weight Goals (Q67)")
+    plt.xlabel("Q66: Perception of Weight (1=Very Underweight to 5=Very Overweight)")
+    plt.ylabel("Number of Students")
+    # show the graph
+    plt.show()
+    
+# ----- GUI -----
+# makes the GUI window
+window = tk.TK()
+# Title of the window
+window.title('GUI')
+# makes a button called "Show Graph" and it runs the graph when clicked on
+btn1 = tk.Button(window, text = 'Show Graph', command = graph)
+btn1.pack()
+
+# keeps the window open until someone closes it
+window.mainloop()
 
 
 
